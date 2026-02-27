75c53e834283f8de355bb74153f2383d3d61968a
krit <krittapasboonruang@gmail.com>
2026-02-27 12:31:47 +0700

    lab 21 completed

--- average.cpp
+++ average.cpp
@@ -1,1 +1,25 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
 
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        cout << "Please input numbers to find average." << endl;
+        return 0;
+    }
+
+    int count = argc - 1;
+    double sum = 0;
+
+    for (int i = 1; i < argc; i++) {
+        sum += atof(argv[i]);
+    }
+
+    double avg = sum / count;
+
+    cout << "---------------------------------" << endl;
+    cout << "Average of " << count << " numbers = " << avg << endl;
+    cout << "---------------------------------" << endl;
+
+    return 0;
+}
